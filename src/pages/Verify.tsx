c1b76d9f197ac4dfef251e1a959b89a7cb387474
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-20 15:20:08 +0400

    wip: add verify page

---
+++ src/pages/Verify.tsx
@@ -0,0 +1,38 @@
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
+
+import { Link } from "react-router-dom";
+
+const BASE_URL = import.meta.env.VITE_BACK_URL;
+
+export default function Verify() {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const hash = params.get("hash");
+
+  useEffect(() => {
+    const verify = async () => {
+      try {
+        await axios.post(`${BASE_URL}/api/verification`, {
+          hash,
+        });
+      } catch (error) {
+        console.error("An error occurred during verification:", error);
+        return error;
+      }
+    };
+    verify();
+  });
+  return (
+    <div>
+      <p>Your eccount verified</p>
+
+      <button>
+        <Link to="/">
+          <p>Log In</p>
+        </Link>
+      </button>
+    </div>
+  );
+}
