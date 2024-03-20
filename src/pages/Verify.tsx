7d0afdde557cec28d65c9b6a93c32f5a90cbd1e6
tchkoidze <ilia.chkoidze12@gmail.com>
2024-03-20 15:26:26 +0400

    fix:link to move login page

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 import { Link } from "react-router-dom";
 
-const BASE_URL = import.meta.env.VITE_BACK_URL;
+//const BASE_URL = import.meta.env.VITE_BACK_URL;
 
 export default function Verify() {
   const location = useLocation();
@@ -14,7 +14,7 @@ export default function Verify() {
   useEffect(() => {
     const verify = async () => {
       try {
-        await axios.post(`${BASE_URL}/api/verification`, {
+        await axios.post(`http://localhost:5000/api/verify`, {
           hash,
         });
       } catch (error) {
@@ -29,7 +29,7 @@ export default function Verify() {
       <p>Your eccount verified</p>
 
       <button>
-        <Link to="/">
+        <Link to="/login">
           <p>Log In</p>
         </Link>
       </button>
